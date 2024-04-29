55639643400ea7862ed0e65e912a35f9ed70b27c
mohamed osama <memeususu111@gmail.com>
2024-04-29 16:40:13 +0300

    finish splash view , navigate to home view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,13 +1,48 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Column(
-      children: [
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToHome();
+  }
 
+  void navigateToHome() {
+     Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(
+          () => const HomeView(),
+          transition: Transition.fade,
+          duration: kTransitionDuration,
+        );
+      },
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          'Reed Free Books',
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
